d6b2b9db8a495c5c7381290e1439d713601fa03b
Jathin-kn <knjathin@gmail.com>
2026-06-05 17:06:11 +0530

    final_inti_test_email

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const cors       = require('cors');
 const app = express();
 
 app.use(cors({
-  origin: 'https://www.thehospitalcare.in'
+  origin: ['https://www.thehospitalcare.in', 'https://thehospitalcare.in']
 }));
 app.use(express.json());
 
